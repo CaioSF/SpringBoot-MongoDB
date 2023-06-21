b0c798d9a0edbd9e8df0743f0fef2bc8ac814481
CaioSF <caios8837@gmail.com>
2023-06-21 11:27:49 -0300

    Adicionando mais documentos

--- src/main/java/com/example/trabalhodaves/TrabalhodavesApplication.java
+++ src/main/java/com/example/trabalhodaves/TrabalhodavesApplication.java
@@ -30,18 +30,30 @@ public class TrabalhodavesApplication implements CommandLineRunner {
     Aluno a1 = new Aluno("1","Marcelo",16);
     Aluno a2 = new Aluno("2","Caio",17);
     Aluno a3 = new Aluno("3","Pedro",18);
+    Aluno a4 = new Aluno("4","Pedro",18);
+    Aluno a5 = new Aluno("5","Chico",20);
+    Aluno a6 = new Aluno("6","João",23);
 
     Nota n1 = new Nota("10",a1,"Matemática",8);
     Nota n2 = new Nota("20",a2,"Ciências",9);
     Nota n3 = new Nota("30",a3,"Português",10);
+    Nota n4 = new Nota("40",a2,"Português",10);
+    Nota n5 = new Nota("50",a2,"Física",10);
+    Nota n6 = new Nota("60",a2,"Química",10);
 
     ar.save(a1);
     ar.save(a2);
     ar.save(a3);
+    ar.save(a4);
+    ar.save(a5);
+    ar.save(a6);
 
     nr.save(n1);
     nr.save(n2);
     nr.save(n3);
+    nr.save(n4);
+    nr.save(n5);
+    nr.save(n6);
 
 
     }
